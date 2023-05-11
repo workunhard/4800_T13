e1e95fe1286a297e82ee4e95200238340ed68a3b
Benny Chao <bchao5@my.bcit.ca>
2023-05-11 15:22:57 -0700

    Initialized default values for input1 and input2 to fix UnboundLocalError

--- pages/views.py
+++ pages/views.py
@@ -6,8 +6,10 @@ from django.views.generic import TemplateView
 
 def homePageView(request):
     output = ''
+    input1 = ''  # Default value
+    input2 = ''  # Default value
     if request.method == 'POST':
-        input1 = request.POST['original']
-        input2 = request.POST['revised']
+        input1 = request.POST.get('original', '')
+        input2 = request.POST.get('revised', '')
         output = input1 + ' ' + input2
     return render(request, 'home.html', {'output': output, 'input1': input1, 'input2': input2})
