0c3b1ea009dbc92ca75aaaec1972f6533d41dcfb
workunhard <workun.code@gmail.com>
2023-05-10 22:36:20 -0700

    Initial commit

---
+++ pages/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.http import Http404
+from django.urls import reverse
+from django.views.generic import TemplateView
+
+def homePageView(request):
+    output = ''
+    if request.method == 'POST':
+        input1 = request.POST['original']
+        input2 = request.POST['revised']
+        output = input1 + ' ' + input2
+    return render(request, 'home.html', {'output': output})
