ad83f30fe15d98d00902a8325aba7a6aa38abff4
Benny Chao <bchao5@my.bcit.ca>
2023-05-19 00:13:23 -0700

    Added dynamic model description in output, depending on models selected. Still requires styling and real model architecture text

--- pages/views.py
+++ pages/views.py
@@ -25,21 +25,33 @@ explanation = {0: "Text is more formal, concise, readable and understandable",
                3: "Better conveys the writer’s writing preferences, including emotions, tone, voice, etc.",
                4: "Updated/added information to the text"}
 
+# Define your models' descriptions
+model_description = {
+    "IteraTeR_ROBERTA": "IteraTeR-RoBERTa is a RoBERTa-based transformer model pre-trained on a large corpus of English text and fine-tuned for the intention classification task. It is designed to understand the text and predict its intention.",
+    "BERT_edit": "BERT_edit is based on the BERT model, which is pre-trained on a large corpus of English text. BERT_edit has been fine-tuned for the task of edit intention prediction, allowing it to understand and predict the intention behind revisions in the text.",
+    "bart_large_mnli": "bart_large_mnli is a model based on the BART architecture and is fine-tuned for the task of natural language inference (NLI). In this context, it is used to understand and classify the intention behind textual revisions."
+}
+
 
 def homePageView(request):
     post_request = False
     post_success = False
-    if request.method == 'POST':
-        post_request = True
-        post_success = True
+    model_choice = ''  # Added initialization
+    comment_model_choice = ''  # Added initialization
     input1 = ''  # Default value
     input2 = ''  # Default value
     input3 = ''  # Default value
     predictions_index = 0  # Default value
     predictions = ''
     bert_predictions = ''  # For the bert model
+    outputs_match = False  # Default value
+
     form = ModelChoiceForm(request.POST or None)
+
     if form.is_valid():
+        post_request = True
+        post_success = True
+
         model_choice = form.cleaned_data.get('model_choice')
         comment_model_choice = form.cleaned_data.get('comment_model_choice')
 
@@ -59,12 +71,14 @@ def homePageView(request):
 
         if comment_model_choice == 'IteraTeR_ROBERTA':
             comment_tokenizer = AutoTokenizer.from_pretrained("wanyu/IteraTeR-ROBERTA-Intention-Classifier")
-            comment_model = AutoModelForSequenceClassification.from_pretrained("wanyu/IteraTeR-ROBERTA-Intention-Classifier")
+            comment_model = AutoModelForSequenceClassification.from_pretrained(
+                "wanyu/IteraTeR-ROBERTA-Intention-Classifier")
             comment_label = {0: "clarity", 1: "coherence", 2: "fluency", 3: "style", 4: "meaning-changed"}
 
         elif comment_model_choice == 'BERT_edit':
             comment_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
-            comment_model = AutoModelForSequenceClassification.from_pretrained("citruschao/bert_edit_intent_classification2")
+            comment_model = AutoModelForSequenceClassification.from_pretrained(
+                "citruschao/bert_edit_intent_classification2")
             comment_label = {0: "clarity", 1: "coherence", 2: "fluency", 3: "meaning-changed", 4: "other", 5: "style"}
 
         elif comment_model_choice == 'bart_large_mnli':
@@ -74,8 +88,11 @@ def homePageView(request):
         input1 = request.POST.get('original', '')
         input2 = request.POST.get('revised', '')
         input3 = request.POST.get('suggested_revision', '')
+
         if input1 == input2:
-            return render(request, 'home.html', {'output': 'No revision detected', 'input1': input1, 'input2': input2, 'input3': input3, 'form': form})
+            return render(request, 'home.html',
+                          {'output': 'No revision detected', 'input1': input1, 'input2': input2, 'input3': input3,
+                           'form': form})
 
         if model_choice == 'IteraTeR_ROBERTA' or model_choice == 'BERT_edit':
             inputs = tokenizer(input1, input2, return_tensors='pt', truncation=True, padding=True)
@@ -88,6 +105,8 @@ def homePageView(request):
 
         elif model_choice == 'bart_large_mnli':
             outputs = classifier(input1 + " " + input2, base_label)
+            predictions_index = outputs['scores'].index(max(outputs['scores']))  # Getting index of max score
+            predictions = outputs['labels'][predictions_index]
 
             print(outputs)
 
@@ -109,21 +128,31 @@ def homePageView(request):
             print(bert_outputs)
             print(comment_model_choice + " prediction: " + bert_predictions)
 
-    outputs_match = predictions == bert_predictions
+            print(outputs)
 
-    print(outputs_match)
+        if predictions == bert_predictions:
+            outputs_match = True
 
-    return render(request, 'home.html',
-                  {'form': form,
-                   'output': 'Predicted revision edit intent: ' + str(predictions),
-                   'explanation': 'Explanation: ' + explanation[predictions_index],
-                   'bert_output': 'Predicted comment edit intent: ' + str(bert_predictions),
-                   'input1': input1,
-                   'input2': input2,
-                   'input3': input3,
-                   'outputs_match': outputs_match,
-                   'post_request': post_request,
-                   'post_success': post_success})
+    context = {
+        'form': form,
+        'output': 'Predicted revision edit intent: ' + str(predictions),
+        'explanation': 'Explanation: ' + explanation[predictions_index],
+        'bert_output': 'Predicted comment edit intent: ' + str(bert_predictions),
+        'input1': input1,
+        'input2': input2,
+        'input3': input3,
+        'outputs_match': outputs_match,
+        'post_request': post_request,
+        'post_success': post_success,
+    }
+
+    # Add model descriptions to the context if the models were selected
+    if model_choice:
+        context['model_description'] = model_description[model_choice]
+    if comment_model_choice:
+        context['comment_model_description'] = model_description[comment_model_choice]
+
+    return render(request, 'home.html', context)
 
 
 def aboutPageView(request):
